4ff6f16899b07eda5f127540b71f882442c42d74
Stuart Young <syoung@edx.org>
2017-06-27 13:28:17 -0400

    add logging to plugin script

--- src/main/groovy/2_install_plugins_via_gradle.groovy
+++ src/main/groovy/2_install_plugins_via_gradle.groovy
@@ -1,6 +1,7 @@
 import jenkins.*;
 import jenkins.model.*;
 import hudson.model.*;
+import java.util.logging.Logger
 @GrabResolver(name='gradle', root='https://repo.gradle.org/gradle/libs-releases-local/')
 @Grapes([
     // slf4j is a runtime depenedency of the gradle tooling api
@@ -8,6 +9,8 @@ import hudson.model.*;
     @Grab(group='org.gradle', module='gradle-tooling-api', version='2.10')
 ])
 import org.gradle.tooling.GradleConnector
+
+Logger logger = Logger.getLogger("")
 
 plugins = Jenkins.instance.pluginManager.plugins
 
@@ -18,10 +21,10 @@ def isInstalled = plugins.any { plugin ->
     plugin.getShortName() == 'ghprb' && plugin.getVersion() == "1.34.0"
 }
 if (isInstalled) {
-    println("BOOTSTRAP: GHPRB present, moving on...:")
+    logger.info("BOOTSTRAP: GHPRB present, moving on...:")
 }
 else {
-    println("BOOTSTRAP: GHPRB not present, installing (w/ dependencies and rebooting!")
+    logger.info("BOOTSTRAP: GHPRB not present, installing (w/ dependencies and rebooting!)")
     def path = new File('/var/lib/jenkins/plugin-resolver')
     def conn = GradleConnector.newConnector().forProjectDirectory(path).connect()
     conn.newBuild().forTasks('clean').run()
