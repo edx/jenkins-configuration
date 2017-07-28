fca59457cac76a9812b756b731449d23ebcb7390
Michael Youngstrom <myoungstrom@edx.org>
2017-07-28 14:26:58 -0400

    Automate importing credentials to Jenkins
    code review changes
    
    Update logging and error catching

---
+++ e2e/test_credential_store_page.py
@@ -0,0 +1,30 @@
+import unittest
+import yaml
+import os
+from bok_choy.web_app_test import WebAppTest
+from pages.credential_store_page import CredentialStorePage
+
+class TestConfiguration(WebAppTest):
+    
+    def setUp(self):
+        super(TestConfiguration, self).setUp()
+        config_path = os.getenv('CONFIG_PATH')
+        try:
+            yaml_contents = open(
+                    "{}/credentials.yml".format(config_path), 'r'
+                    ).read()
+        except IOError:
+            pass
+        self.credentials = yaml.load(yaml_contents)
+        self.credential_store_page = CredentialStorePage(self.browser)
+
+    def test_credential_store(self):
+        """
+        Test the credential store table for the test_data credentials.
+        """
+        credential_store_page = self.credential_store_page.visit()
+        assert len(self.credentials) == credential_store_page.get_number_credentials()
+        for cred in self.credentials:
+            # Make sure each credentials description exists in the table. This is the
+            # only info consistently shown for all credential types.
+            assert cred['description'] in credential_store_page.get_list_of_credentials_table()
---
+++ src/main/groovy/6importCredentials.groovy
@@ -0,0 +1,255 @@
+/**
+*
+* Automate adding credentials into Jenkins
+*
+**/
+import java.util.logging.Logger
+import jenkins.*
+import jenkins.model.*
+import hudson.*
+import hudson.model.*
+import hudson.util.Secret
+
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.common.*
+import com.cloudbees.plugins.credentials.domains.*
+import com.cloudbees.plugins.credentials.impl.*
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+import org.jenkinsci.plugins.plaincredentials.impl.*
+
+import org.apache.commons.fileupload.*
+import org.apache.commons.fileupload.disk.*
+import java.nio.file.*
+
+@Grapes([
+    @Grab(group='org.yaml', module='snakeyaml', version='1.17')
+])
+import org.yaml.snakeyaml.Yaml
+
+Logger logger = Logger.getLogger("")
+Jenkins jenkins = Jenkins.getInstance()
+Yaml yaml = new Yaml()
+
+globalDomain = Domain.global()
+credentialsStore =
+Jenkins.instance.getExtensionList(
+    'com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
+
+String configPath = System.getenv("JENKINS_CONFIG_PATH")
+String configText = ''
+try {
+    configText = new File("${configPath}/credentials.yml").text
+} catch (FileNotFoundException e) {
+    logger.severe("Cannot find config file path @ ${configPath}/credentials.yml")
+    jenkins.doSafeExit(null)
+    System.exit(1)
+}
+
+// Delete any existing credentials to avoid clutter
+// and unintended duplication.
+credDomains = credentialsStore.getDomains()
+credDomains.each { domain ->
+    allCredentials = credentialsStore.getCredentials(domain)
+    allCredentials.each { credential ->
+        credentialsStore.removeCredentials(domain, credential)
+    }
+    // Now remove the domain
+    credentialsStore.removeDomain(domain)
+}
+
+List credentialConfig = yaml.load(configText)
+credentialConfig.each { newCredential ->
+    credentialType = newCredential.credentialType
+    id = newCredential.id
+    description = newCredential.description
+    scopeString = newCredential.scope
+
+    // All credentials must have a credentialType and scope
+    if (!credentialType || !scopeString) {
+        logger.severe("Missing data for credential. Please ensure every entry in the " +
+                      "credentials.yml file has a credentialType and scope")
+        jenkins.doSafeExit(null)
+        System.exit(1)
+    }
+
+    switch (scopeString.toLowerCase()) {
+        case 'global':
+            scope = CredentialsScope.GLOBAL
+            break
+        case 'system':
+            scope = CredentialsScope.SYSTEM
+            break
+        default:
+            logger.severe("Invalid scope specified for credential. Scope " +
+                          "should be GLOBAL or SYSTEM. Was: ${scopeString}")
+            jenkins.doSafeExit(null)
+            System.exit(1)
+    }
+
+    switch (credentialType) {
+        case 'usernamePassword':
+            username = newCredential.username
+            password = newCredential.password
+
+            // Make sure the username and password fields are not empty
+            if (!username || !password) {
+                logger.severe("Missing data for credential. Please ensure usernamePassword " +
+                              "entries in the credentials.yml file have a " +
+                              "username and password")
+                jenkins.doSafeExit(null)
+                System.exit(1)
+            }
+
+            cred = new UsernamePasswordCredentialsImpl(
+                scope,
+                id,
+                description,
+                username,
+                password
+            )
+            credentialsStore.addCredentials(
+                globalDomain,
+                cred
+            )
+            break
+        case 'secretText':
+            text = newCredential.secretText
+
+            // Make sure the text field is not empty
+            if (!text) {
+                logger.severe("Missing data for credential. Please ensure secretText " +
+                              "entries in the credentials.yml file have text")
+                jenkins.doSafeExit(null)
+                System.exit(1)
+            }
+
+            secretText = new StringCredentialsImpl(
+                scope,
+                id,
+                description,
+                Secret.fromString(text)
+            )
+            credentialsStore.addCredentials(globalDomain, secretText)
+            break
+        case 'secretFile':
+            path = newCredential.path
+            fileName = newCredential.name
+
+            // Make sure the path and name is not empty
+            if (!path || !fileName) {
+                logger.severe("Missing data for credential. Please ensure secretFile " +
+                              "entries in the credentials.yml file have a path and name")
+                jenkins.doSafeExit(null)
+                System.exit(1)
+            }
+
+            factory = new DiskFileItemFactory()
+            fileItem = factory.createItem("", "application/octet-stream", false, fileName)
+            out = fileItem.getOutputStream()
+            file = new File(path)
+            try {
+                Files.copy(file.toPath(), out)
+            } catch (NoSuchFileException e) {
+                logger.severe("No file found at: ${path}. Please ensure the file exists " +
+                              "and try again.")
+                jenkins.doSafeExit(null)
+                System.exit(1)
+            }
+            secretFile = new FileCredentialsImpl(
+                scope,
+                id,
+                description,
+                fileItem,
+                "",
+                ""
+            )
+            credentialsStore.addCredentials(globalDomain, secretFile)
+            break
+        case 'ssh':
+            username = newCredential.username
+            passphrase = newCredential.passphrase
+            isJenkinsMasterSsh = newCredential.isJenkinsMasterSsh
+
+            // Make sure username, passphrase and isJenkinsMasterSsh are not empty
+            if (!username || !passphrase || isJenkinsMasterSsh == null) {
+                logger.severe("Missing data for credential. Please ensure ssh " +
+                              "entries in the credentials.yml file have a username, " +
+                              "passphrase, and isJenkinsMasterSsh")
+                jenkins.doSafeExit(null)
+                System.exit(1)
+            }
+
+            if (isJenkinsMasterSsh) {
+                sshKey = new BasicSSHUserPrivateKey.UsersPrivateKeySource()
+            } else {
+                sshPath = newCredential.path
+
+                // Make sure there is a path to the ssh file
+                if (!sshPath) {
+                    logger.severe("Missing data for credential. Please ensure ssh " +
+                                  "entries in the credentials.yml file " +
+                                  "(with isJenkinsMasterSsh = False) have a " +
+                                  "path")
+                    jenkins.doSafeExit(null)
+                    System.exit(1)
+                }
+
+                // Make sure the ssh file exists
+                sshFileExists = Files.exists(Paths.get(sshPath), LinkOption.NOFOLLOW_LINKS)
+                if (!sshFileExists) {
+                    logger.severe("No ssh file found at: ${sshPath}. Please ensure the file " +
+                                  "exists and try again.")
+                    jenkins.doSafeExit(null)
+                    System.exit(1)
+                }
+
+                sshKey = new BasicSSHUserPrivateKey.FileOnMasterPrivateKeySource(sshPath)
+            }
+            ssh = new BasicSSHUserPrivateKey(
+                scope,
+                id,
+                username,
+                sshKey,
+                passphrase,
+                description
+            )
+            credentialsStore.addCredentials(globalDomain, ssh)
+            break
+        case 'certificate':
+            certPath = newCredential.path
+            password = newCredential.password
+
+            // Make sure the path is not empty
+            if (!certPath || !password) {
+                logger.severe("Missing data for credential. Please ensure certificate " +
+                              "entries in the credentials.yml file have a path and password")
+                jenkins.doSafeExit(null)
+                System.exit(1)
+            }
+
+            // Make sure the certificate file exists
+            certFileExists = Files.exists(Paths.get(certPath), LinkOption.NOFOLLOW_LINKS)
+            if (!certFileExists) {
+                logger.severe("No certificate file found at: ${certPath}. Please ensure the file " +
+                              "exists and try again.")
+                jenkins.doSafeExit(null)
+                System.exit(1)
+            }
+
+            cert = new CertificateCredentialsImpl.FileOnMasterKeyStoreSource(certPath)
+            certificate = new CertificateCredentialsImpl(
+                scope,
+                id,
+                description,
+                password,
+                cert
+            )
+            credentialsStore.addCredentials(globalDomain, certificate)
+            break
+        default:
+            logger.severe("Invalid credentialType. Must be usernamePassword, secretText, " +
+                          "secretFile, ssh, or certificate. Got: ${credentialType}")
+            jenkins.doSafeExit(null)
+            System.exit(1)
+    }
+}
