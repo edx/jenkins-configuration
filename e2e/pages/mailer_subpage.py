df235ba1e0251aef444e2c2d24d12d38617c71b5
Stuart Young <syoung@edx.org>
2017-08-11 11:10:22 -0400

    add script to configure the mailer plugin

---
+++ e2e/pages/mailer_subpage.py
@@ -0,0 +1,25 @@
+from bok_choy.page_object import PageObject
+
+class MailerConfigurationSubPage(PageObject):
+
+    url = 'http://localhost:8080/configure'
+
+    def is_browser_on_page(self):
+        self.scroll_to_element('[name="_.smtpServer"]', timeout=10)
+        return self.q(css='.setting-input.validated[name="_.smtpServer"]').visible
+
+    @property
+    def nameref_id(self):
+        return self.q(css=
+                '[name="hudson-tasks-Mailer"]'
+                ).attrs('id')[0]
+
+    def expand_advanced(self):
+        css_query='[nameref="{}"] > td > div.advancedLink > span.yui-button'.format(self.nameref_id)
+        self.q(css=css_query).click()
+
+    def get_smtp_server(self):
+        return self.q(css='[name="_.smtpServer"]').attrs('value')[0]
+
+    def get_smtp_port(self):
+        return self.q(css='[name="_.smtpPort"]').attrs('value')[0]
---
+++ e2e/test_mailer_configuration.py
@@ -0,0 +1,27 @@
+import unittest
+import yaml
+import os
+from bok_choy.web_app_test import WebAppTest
+from pages.mailer_subpage import MailerConfigurationSubPage
+
+class TestMailerConfiguration(WebAppTest):
+    
+    def setUp(self):
+        super(TestMailerConfiguration, self).setUp()
+        config_path = os.getenv('CONFIG_PATH')
+        try:
+            yaml_contents = open(
+                    "{}/mailer_config.yml".format(config_path), 'r'
+                    ).read()
+        except IOError:
+            pass
+        self.mailer_config = yaml.load(yaml_contents)
+        self.config_page = MailerConfigurationSubPage(self.browser)
+
+    def test_mailer_config(self):
+        self.config_page.visit()
+        # test a component of the main mailer cofig
+        assert self.mailer_config['SMTP_SERVER'] == self.config_page.get_smtp_server()
+        self.config_page.expand_advanced()
+        # test an advanced component of the mailer config
+        assert str(self.mailer_config['SMTP_PORT']) == self.config_page.get_smtp_port()
