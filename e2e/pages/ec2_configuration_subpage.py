ba40e6e7850bd562f6f7a466ddd88b82207763c4
Michael Youngstrom <myoungstrom@edx.org>
2017-08-17 10:34:47 -0400

    add e2e test

--- e2e/pages/ec2_configuration_subpage.py
+++ e2e/pages/ec2_configuration_subpage.py
@@ -6,17 +6,31 @@ class Ec2ConfigurationSubPage(PageObject):
     url = "http://{}:8080/configure".format(JENKINS_HOST)
 
     def is_browser_on_page(self):
-        self.scroll_to_element('[name="_.cloudName"]', timeout=10)
         return self.q(css='[name="_.cloudName"]').visible
 
-    @property
-    def nameref_id(self):
-        return self.q(css=
-                '[name="jenkins-model-GlobalCloudConfiguration"]'
-                ).attrs('id')[0]
+    def get_cloud_names(self):
+        return self.q(css='[name="_.cloudName"]').attrs('value')
 
-    def get_cloud_name(self):
-        return self.q(css='[name="_.cloudName"]').attrs('value')[0]
+    def get_cloud_regions(self):
+        return self.q(css='[name="_.region"]').attrs('value')
 
-    def get_cloud_region(self):
-        return self.q(css='[name="_.region"]').attrs('value')[0]
+    def get_cloud_access_keys(self):
+        return self.q(css='[name="_.accessId"]').attrs('value')
+
+    def get_ami_descriptions(self):
+        return self.q(css='[name="_.description"]').attrs('value')
+
+    def get_ami_zones(self):
+        return self.q(css='[name="_.zone"]').attrs('value')
+
+    def get_ami_ids(self):
+        return self.q(css='[name="_.ami"]').attrs('value')
+
+    def get_fs_roots(self):
+        return self.q(css='[name="_.remoteFS"]').attrs('value')
+
+    def get_ssh_ports(self):
+        return self.q(css='[name="_.sshPort"]').attrs('value')
+
+    def get_idle_termination_times(self):
+        return self.q(css='[name="_.idleTerminationMinutes"]').attrs('value')
--- e2e/test_ec2_configuration_page.py
+++ e2e/test_ec2_configuration_page.py
@@ -19,9 +19,34 @@ class TestEc2ConfigurationSubPage(WebAppTest):
         self.ec2_cloud_config = self.ec2_config['CLOUDS']
         self.config_page = Ec2ConfigurationSubPage(self.browser)
 
-    def test_ghprb_config(self):
+    def test_ec2_config(self):
         """
-        verify a couple of the configuration options of the EC2 plugin from
+        Verify a couple of the configuration options of the EC2 plugin from
         the Jenkins configuration console
         """
         self.config_page.visit()
+
+        # Since there may be several clouds and amis, get a list
+        # of the configurable settings and make sure content from
+        # the yaml file exists in them.
+        cloud_names = self.config_page.get_cloud_names()
+        cloud_regions = self.config_page.get_cloud_regions()
+        cloud_access_keys = self.config_page.get_cloud_access_keys()
+        ami_descriptions = self.config_page.get_ami_descriptions()
+        ami_ids = self.config_page.get_ami_ids()
+        ami_zones = self.config_page.get_ami_zones()
+        ssh_ports = self.config_page.get_ssh_ports()
+        fs_roots = self.config_page.get_fs_roots()
+        idle_termination_times = self.config_page.get_idle_termination_times()
+
+        for cloud in self.ec2_cloud_config:
+            assert cloud["NAME"] in cloud_names
+            assert cloud["REGION"] in cloud_regions
+            assert cloud["ACCESS_KEY_ID"] in cloud_access_keys
+            for ami in cloud["AMIS"]:
+                assert ami["AMI_ID"] in ami_ids
+                assert ami["DESCRIPTION"] in ami_descriptions
+                assert ami["AVAILABILITY_ZONE"] in ami_zones
+                assert ami["SSH_PORT"] in ssh_ports
+                assert ami["REMOTE_FS_ROOT"] in fs_roots
+                assert ami["IDLE_TERMINATION_MINUTES"] in idle_termination_times
