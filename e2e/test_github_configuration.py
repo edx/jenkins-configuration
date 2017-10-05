0e1cc58dc40312922372c3379dde647432eba218
Stuart Young <syoung@edx.org>
2017-10-05 11:38:16 -0400

    add script and tests for gh plugin

---
+++ e2e/test_github_configuration.py
@@ -0,0 +1,27 @@
+import unittest
+import yaml
+import os
+from bok_choy.web_app_test import WebAppTest
+from pages.github_configuration_subpage import GithubConfigurationSubPage
+
+class TestGithubConfiguration(WebAppTest):
+    
+    def setUp(self):
+        super(TestGithubConfiguration, self).setUp()
+        config_path = os.getenv('CONFIG_PATH')
+        try:
+            yaml_contents = open(
+                    "{}/github_config.yml".format(config_path), 'r'
+                    ).read()
+        except IOError:
+            pass
+        self.github_config = yaml.load(yaml_contents)
+        self.config_page = GithubConfigurationSubPage(self.browser)
+
+    def test_github_config(self):
+        self.config_page.visit()
+        # test a component of the main ggithub config
+        assert self.github_config[0]['API_URL'] == self.config_page.get_api_url()
+        self.config_page.expand_advanced()
+        # test an advanced component of the github config
+        assert str(self.github_config[0]['CACHE_SIZE']) == self.config_page.get_cache_size()
---
+++ src/main/groovy/4configureGithub.groovy
@@ -0,0 +1,43 @@
+import java.util.logging.Logger
+
+import jenkins.*
+import jenkins.model.*
+
+import org.jenkinsci.plugins.github.GitHubPlugin
+import org.jenkinsci.plugins.github.config.GitHubServerConfig
+
+@Grapes([
+    @Grab(group='org.yaml', module='snakeyaml', version='1.17')
+])
+import org.yaml.snakeyaml.Yaml
+
+Logger logger = Logger.getLogger("")
+Jenkins jenkins = Jenkins.getInstance()
+Yaml yaml = new Yaml()
+
+String configPath = System.getenv("JENKINS_CONFIG_PATH")
+try {
+    configText = new File("${configPath}/github_config.yml").text
+} catch (FileNotFoundException e) {
+    logger.severe("Cannot find config file path @ ${configPath}/github_config.yml")
+    jenkins.doSafeExit(null)
+    System.exit(1)
+}
+
+List githubConfigs = yaml.load(configText)
+
+githubConfigs.each { config ->
+    GitHubServerConfig server = new GitHubServerConfig(config.CREDENTIAL_ID)
+    server.setManageHooks(config.MANAGE_HOOKS.toBoolean())
+    if (config.USE_CUSTOM_API_URL.toBoolean()) {
+        server.setCustomApiUrl(config.USE_CUSTOM_API_URL.toBoolean())
+        server.setApiUrl(config.API_URL)
+    }
+    if (config.CACHE_SIZE > 0) {
+        server.setClientCacheSize(config.CACHE_SIZE)
+    }
+    GitHubPlugin.configuration().getConfigs().add(server)
+}
+
+// Note: This script does not currently include configuration for advanced
+// github plugin options
