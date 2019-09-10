540022b080b68a11ce38afdfd212f2be14f9764e
Jeremy Bowman <jbowman@edx.org>
2019-09-10 11:37:18 -0400

    TE-2854 Upgrade Firefox, bok-choy, and selenium (#138)

--- e2e/pages/configuration_page.py
+++ e2e/pages/configuration_page.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.action_chains import ActionChains
+
 from . import JENKINS_HOST
 from bok_choy.page_object import PageObject
 
@@ -6,7 +8,8 @@ class JenkinsConfigurationPage(PageObject):
     url = "http://{}:8080/configure".format(JENKINS_HOST)
 
     def expand_advanced(self):
-        self.q(css='[class="advancedLink"] > span > span > button').first.click()
+        element = self.browser.find_element_by_css_selector('[class="advancedLink"] > span > span > button')
+        ActionChains(self.browser).move_to_element(element).click(element)
 
     def is_browser_on_page(self):
         return 'configure system [jenkins]' in self.browser.title.lower()
@@ -50,9 +53,9 @@ class ConfigurationSubPageMixIn(object):
     def expand_advanced(self):
         """ Press the "Advanced" button for the plugin section."""
         nameref_id = self.q(css='[name="{}"]'.format(self.name)).attrs('id')[0]
-        css_query='[nameref="{}"] > td > div.advancedLink > span.yui-button'.format(nameref_id)
-        self.scroll_to_element(css_query)
-        self.q(css=css_query).click()
+        css_query = '[nameref="{}"] > td > div.advancedLink > span.yui-button button'.format(nameref_id)
+        element = self.browser.find_element_by_css_selector(css_query)
+        ActionChains(self.browser).move_to_element(element).click(element)
 
     def values_of_elements_named(self, el_name):
         """
