19aa5ba75fbd380352712e18d5dad380b649a12b
nadeemshahzad <nshahzad@edx.org>
2020-04-02 17:57:18 +0500

    fix

--- scripts/scanPluginUpgrades.groovy
+++ scripts/scanPluginUpgrades.groovy
@@ -8,9 +8,7 @@
 import jenkins.security.UpdateSiteWarningsConfiguration
 import groovy.json.StreamingJsonBuilder
 
-import com.atlassian.jira.bc.issue.IssueService
-import com.atlassian.jira.component.ComponentAccessor
-
+import com.atlassian.jira.bc.issue
 // Return a list of Security Warnings from the Jenkins update center
 // that are pertinent to the plugins installed in this Jenkins instance
 public Set getSecurityWarnings() {
