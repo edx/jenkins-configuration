7923b98010f619ae9b45c956da664c18562993b8
nadeemshahzad <nshahzad@edx.org>
2020-04-02 14:42:54 +0500

    fix

--- scripts/scanPluginUpgrades.groovy
+++ scripts/scanPluginUpgrades.groovy
@@ -7,6 +7,7 @@
 
 import jenkins.security.UpdateSiteWarningsConfiguration
 
+
 // Return a list of Security Warnings from the Jenkins update center
 // that are pertinent to the plugins installed in this Jenkins instance
 public Set getSecurityWarnings() {
@@ -26,30 +27,7 @@ public static void main(String[] args) {
     warnings.each { warning ->
         println(formatWarning(warning))
     }
-}
+   
+    
 
-import groovy.json.StreamingJsonBuilder
-
-
-def authString = "nadeem.shahzad@arbisoft.com:3aINhIFy5qzRHC9rrlYX0CF5".getBytes().encodeBase64().toString()
-def body_req = '''{
-   "fields": {
-     "project" : { "key" : "DEVOPS" },
-     "issuetype" : { "name" : "Incident" },
-     "summary" : "test",
-     "description" : "test"}
- }'''
-
-def connection = new URL("https://arbisoft123.atlassian.net/rest/api/2/issue/").openConnection() as HttpURLConnection
-connection.setRequestMethod( "POST" )
-connection.setRequestProperty( "Authorization", "Basic ${authString}" )
-connection.doOutput = true
-connection.setRequestProperty("Content-Type", "application/json;charset=UTF-8")
-/*connection.outputStream.withWriter("UTF-8") { new StreamingJsonBuilder(it, body_req) }*/
-connection.getOutputStream().write(body_req.getBytes("UTF-8"))
-connection.connect()
-def postRC = connection.getResponseCode();
-println(postRC);
-if(postRC.equals(200)) { 
-println(connection.getInputStream().getText());
 }
