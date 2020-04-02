0931cc246bbf61b07af032ee0545d0dbe06d0f43
nadeemshahzad <nshahzad@edx.org>
2020-04-03 00:29:26 +0500

    fix

--- scripts/scanPluginUpgrades.groovy
+++ scripts/scanPluginUpgrades.groovy
@@ -55,6 +55,6 @@ def jira_url_search = "https://arbisoft123.atlassian.net/rest/api/2/search/?jql=
 def proc = [ "curl", "-u", "${authString}", "-X", "GET", "-H", "Content-Type: application/json", "${jira_url_search}"].execute()
 
 println(proc.getClass())
-println(proc.text)
+println(proc.text['issues'])
 
 }
