a308bdd6a36e2a272fdf5efe066bc24ac522c7ff
nadeemshahzad <nshahzad@edx.org>
2020-04-02 16:54:39 +0500

    fix

--- scripts/scanPluginUpgrades.groovy
+++ scripts/scanPluginUpgrades.groovy
@@ -39,11 +39,13 @@ def body_req = '''{
  }'''
 
 
+def url = "https://arbisoft123.atlassian.net/rest/api/2/issue/"
 
 try {
       
 
-def response = "curl -D- -u ${authString} -X POST --data '${body_req}' -H \"Content-Type: application/json\" https://arbisoft123.atlassian.net/rest/api/2/issue/"
+
+def response = "curl -D- -u ${authString} -X POST --data '${body_req}' -H \"Content-Type: application/json\" ${url}".execute()
 
 
 println(response);
