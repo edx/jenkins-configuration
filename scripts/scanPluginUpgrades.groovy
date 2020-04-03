35ea1f113e63defd23e83a146eef0dac9a1da8a0
nadeemshahzad <nshahzad@edx.org>
2020-04-03 09:57:03 +0500

    fix

--- scripts/scanPluginUpgrades.groovy
+++ scripts/scanPluginUpgrades.groovy
@@ -35,7 +35,7 @@ public static void main(String[] args) {
             "project" : { "key" : "DEVOPS" },
             "issuetype" : { "name" : "Bug" },
             "summary" : "Build Jenkins Security Check",
-            "description" : "${description}" }
+            "description" : "description" }
     }"""
 
     def jira_url = "https://arbisoft123.atlassian.net/rest/api/2/issue/"
