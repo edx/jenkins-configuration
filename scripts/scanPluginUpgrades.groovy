50beb27a3e01cec23e4d7aa914fba43d751ac3a1
nadeemshahzad <nshahzad@edx.org>
2020-04-03 11:03:38 +0500

    fix

--- scripts/scanPluginUpgrades.groovy
+++ scripts/scanPluginUpgrades.groovy
@@ -29,8 +29,8 @@ public static void main(String[] args) {
         description += formatWarning(warning)
     }
     description += """\\nImplementation Details:\\nhttps://openedx.atlassian.net/
-                   wiki/spaces/EdxOps/pages/1062895636/How+to+update+jenkins+plugins+for+https+build.testeng.edx.org\\n
-                   https://openedx.atlassian.net/wiki/spaces/EdxOps/pages/1062895636/How+to+update+jenkins+plugins+for+https+build.testeng.edx.org"""
+wiki/spaces/EdxOps/pages/1062895636/How+to+update+jenkins+plugins+for+https+build.testeng.edx.org\\n
+https://openedx.atlassian.net/wiki/spaces/EdxOps/pages/1062895636/How+to+update+jenkins+plugins+for+https+build.testeng.edx.org"""
     println(description)
     def authString = "nadeem.shahzad@arbisoft.com:3aINhIFy5qzRHC9rrlYX0CF5"
     def req = """{
