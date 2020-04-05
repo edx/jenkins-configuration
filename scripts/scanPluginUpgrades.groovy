489cd7b5ec4db5d6faa92f18d94f76ce6d84800f
nadeemshahzad <nshahzad@edx.org>
2020-04-05 23:59:48 +0500

    fix

--- scripts/scanPluginUpgrades.groovy
+++ scripts/scanPluginUpgrades.groovy
@@ -33,7 +33,8 @@ wiki/spaces/EdxOps/pages/1062895636/How+to+update+jenkins+plugins+for+https+buil
 https://openedx.atlassian.net/wiki/spaces/EdxOps/pages/1062895636/How+to+update+jenkins+plugins+for+https+build.testeng.edx.org"""
     println(description)
   //  def authString = "nadeem.shahzad@arbisoft.com:3aINhIFy5qzRHC9rrlYX0CF5"
-    def authString = "${JIRA_USER}:${JIRA_PASSWORD}"
+    println env['JIRA_USER']
+    def authString = env['JIRA_USER']:env['JIRA_PASSWORD']
 
 println("${authString}")
 
