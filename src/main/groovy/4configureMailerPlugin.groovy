4a62b050890e1e43d868ebfeed59a93359f708bd
Christopher Pappas <cpappas@edx.org>
2020-11-12 10:32:28 -0500

    iterating on groovy script

--- src/main/groovy/4configureMailerPlugin.groovy
+++ src/main/groovy/4configureMailerPlugin.groovy
@@ -14,6 +14,7 @@ import org.kohsuke.stapler.*;
 
 @Grapes([
     @Grab(group='org.yaml', module='snakeyaml', version='1.17')
+    @Grab(group='org.mockito', module='mockito-all', version='1.10.19')
 ])
 import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.constructor.SafeConstructor
